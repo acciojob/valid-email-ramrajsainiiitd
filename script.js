36d5ba30be45e3a568a685b50d0fc564d07ca9ca
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-13 23:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function validEmail(str) {
-  //your JS code here.
+  let email = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9]+.[a-z]{2,3}.[a-z]{2,3}$/;
+return email.test(str);
 }
 
-// Do not change the code below.
+// Do not change the code below.   
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
